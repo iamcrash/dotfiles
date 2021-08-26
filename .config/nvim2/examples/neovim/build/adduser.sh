abb903ebea164c8820858e13df3d36911279f489
iamcrash <kroni.dev@icloud.com>
2021-08-26 15:13:55 +0000

    init

--- .config/nvim2/examples/neovim/build/adduser.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-USERNAME=$(whoami)
-UID=$UID
-USER_GROUP=staff
-GID=$GID
-
-echo "add user:${USERNAME}"
-addgroup -g ${USER_GID} ${USER_GROUP}
-adduser -u ${USER_UID} -G ${USER_GROUP} -D -s /bin/zsh ${USER}
--- .config/nvim2/install/install-nightly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~ || exit 1
-rm -r neovim || true
-git clone https://github.com/neovim/neovim
-cd neovim || exit 1
-make CMAKE_BUILD_TYPE=Release install
-cd ~ || exit 1
-rm -r neovim
