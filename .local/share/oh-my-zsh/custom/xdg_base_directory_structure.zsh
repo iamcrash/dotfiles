5ced6b8d98774cb0b72af3685df247c2f8c0a7b2
iamcrash <kroni.dev@icloud.com>
2021-08-26 17:09:27 +0000

    init

--- .local/share/oh-my-zsh/custom/xdg_base_directory_structure.zsh
+++ .local/share/oh-my-zsh/custom/xdg_base_directory_structure.zsh
@@ -1,25 +1,9 @@
-export XDG_DESKTOP_DIR=$HOME/Desktop
-
-export XDG_DOWNLOAD_DIR=$HOME/Downloads
-
-# defines the base directory relative to which user-specific non-essential data files should be stored
 export XDG_CACHE_HOME=$HOME/.cache
-
 # defines the base directory relative to which user-specific configuration files should be stored
 export XDG_CONFIG_HOME=$HOME/.config
-
-#  single base directory relative to which user-specific data files should be written
-export XDG_DATA_HOME=$HOME/.data
-
+# single base directory relative to which user-specific data files should be written
+export XDG_DATA_HOME=$HOME/.local/share
 # defines the base directory relative to which user-specific state files should be stored
 export XDG_STATE_HOME=$HOME/.state
-
-#
-# DIRS
-#
-
-# # set of preference ordered base directories relative to which data files should be searched
-# export XDG_DATA_DIRS="${HOME}/.local/share:${HOME}/.local/.share"
-
-# # defines the base directory relative to which user-specific non-essential runtime files and other file objects (such as sockets, named pipes, ...) should be stored.
-export XDG_RUNTIME_DIR=$HOME/.runtime
+# defines the base directory relative to which user-specific non-essential runtime files and other file objects (such as sockets, named pipes, ...) should be stored.
+export XDG_RUNTIME_DIR=/run/user/$UID
