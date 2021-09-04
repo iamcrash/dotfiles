9ec22e42b4beda3631836cf26f5a64ac187f8419
Crash <kronic.dev@icloud.com>
2021-09-04 20:27:13 +0000

    init

--- install.zsh
+++ install.zsh
@@ -1,13 +1,32 @@
-git pull --branch dev-advanced https://github.com/iamcrash/dotfiles \
-&& cd dotfiles \
-&& rsync -avh \
+mkdir -p ~/.config
+
+env
+
+cd ~/.config
+
+DOTFILESDIR=~/.config/dotfiles
+
+git clone --branch dev-advanced https://github.com/iamcrash/dotfiles
+
+# if [ -d "${DOTFILESDIR}" ] 
+# then
+#     cd dotfiles
+#     git pull --branch dev-advanced https://github.com/iamcrash/dotfiles
+# else
+#     git clone --branch dev-advanced https://github.com/iamcrash/dotfiles
+# fi
+
+rsync -avh \
   --no-perms \
   --exclude "old/" \
   --exclude ".DS_Store" \
   --exclude ".git/" \
-  . .. \
-&& cd .. \
-&& source zsh/.zprofile \
-&& env > test.env
+  . ..
+
+cd .. \
+
+source zsh/.zprofile \
+
+env
 
 # ZSH=${ZSH} curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | zsh || true
